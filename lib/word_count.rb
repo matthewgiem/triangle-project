6ee4a934811e33faab32f584070e08b806dd864b
Matthew Giem <matthew.giem@gmail.com>
2016-07-01 09:02:41 -0700

    create the method word_evaluate and make it pass the first two specs (tell you if two words are the same)

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -3,3 +3,12 @@ class String
   define_method(:word_count) do
   end
 end
+class String
+  define_method(:word_evaluator) do |word|
+    if word == self
+      true
+    else
+      false
+    end
+  end
+end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -2,8 +2,12 @@
 require("rspec")
 require("word_count.rb")
 
-describe('String#word_count') do
-  it('') do
-    expect("".word_count("")).to(eq(""))
+describe('String#word_evaluator') do
+  it('recognize if two words are the same') do
+    expect("hello".word_evaluator("hello")).to(eq(true))
   end
+  it('recognize if two words are not the same') do
+    expect("hello".word_evaluator("earth")).to(eq(false))
+  end
+
 end
