2f4a7f036f8a0ebb776542c3501a54b121e7b342
Matthew Giem <matthew.giem@gmail.com>
2016-07-01 08:39:27 -0700

    create the README.md file

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -1,14 +1,14 @@
-require('capybara/rspec')
-require('./app')
-Capybara.app = Sinatra::Application
-set(:show_exceptions, false)
-
-describe('', {:type => :feature}) do
-  it('') do
-    visit('/')
-    fill_in('', :with => '')
-    fill_in('', :with => '')
-    click_button('')
-    expect(page).to have_content("")
-  end
-end
+# require('capybara/rspec')
+# require('./app')
+# Capybara.app = Sinatra::Application
+# set(:show_exceptions, false)
+#
+# describe('', {:type => :feature}) do
+#   it('') do
+#     visit('/')
+#     fill_in('', :with => '')
+#     fill_in('', :with => '')
+#     click_button('')
+#     expect(page).to have_content("")
+#   end
+# end
